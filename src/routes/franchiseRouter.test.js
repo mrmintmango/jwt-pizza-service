6974c6b7270f2e8fc688879e18bd508d3e6897d9
MrMintCode <Ruben.Matos.imp@gmail.com>
2025-09-23 16:27:22 -0600

    add(ci) build and lint

---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,10 @@
+// const { Role, DB } = require('../database/database.js');
+
+// async function createAdminUser() {
+//   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + '@admin.com';
+
+//   user = await DB.addUser(user);
+//   return { ...user, password: 'toomanysecrets' };
+// }
